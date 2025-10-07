3a39348a0b6b581903304c6ce71e8b8b1ed29984
Arseniy Uspenskiy <uspenskiyarseniy@gmail.com>
2025-10-07 19:58:45 +0200

    some movement progresss

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    [SerializeField] private Rigidbody2D rigidbody2;
+    [SerializeField] private Rigidbody2D playerBody;
+    [SerializeField] private CapsuleCollider2D playerCollider2D;
+    [SerializeField] private LayerMask platformLayerMask;
 
     // Start is called before the first frame update
     private void Start()
@@ -18,11 +20,17 @@ public class Player : MonoBehaviour
     {
 
         if (Input.GetKeyDown(KeyCode.Space))
-        {
+        { 
             float jumpVelocity = 30f;
-            rigidbody2.velocity = Vector2.up * jumpVelocity;
-
+            playerBody.velocity = Vector2.up * jumpVelocity;
         }
         
     }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.CapsuleCast(playerCollider2D.bounds.center, playerCollider2D.bounds.size, 0f, 0f, Vector2.down * .1f);
+        return raycastHit2d.collider != null;
+    }
+
 }
