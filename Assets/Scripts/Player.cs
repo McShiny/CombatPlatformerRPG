1f1b1ef19ae57b66aec06745d99d4f4336898196
sunaina <sunainabattula1@gmail.com>
2025-10-09 12:27:27 +0200

    left to right movement yippiee

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,24 +5,36 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    [SerializeField] private Rigidbody2D rigidbody2;
+    [SerializeField] private Rigidbody2D playerBody;
+    public float speed = 5f;
+    private Vector2 movement;
 
     // Start is called before the first frame update
     private void Start()
     {
-        
     }
 
     // Update is called once per frame
     private void Update()
     {
+        float input = Input.GetAxisRaw("Horizontal");
+        movement.x = input * speed * Time.deltaTime;
+        transform.Translate(movement);
 
+        if (input != 0)
+        {
+        Vector3 localScale = transform.localScale;
+        localScale.x = Mathf.Sign(input) * Mathf.Abs(localScale.x);
+        transform.localScale = localScale;
+        } 
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             float jumpVelocity = 30f;
-            rigidbody2.velocity = Vector2.up * jumpVelocity;
+            playerBody.velocity = Vector2.up * jumpVelocity;
 
         }
+
         
     }
 }
