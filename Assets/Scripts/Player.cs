bd93e3b33c95a02ea371b3d0e21995a29d29db0d
Arseniy Uspenskiy <uspenskiyarseniy@gmail.com>
2026-02-05 20:31:54 +0200

    improved Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,47 +11,90 @@ public class Player : MonoBehaviour
     [SerializeField] private LayerMask platformLayerMask;
 
     private float moveSpeed = 5f;
+    private float moveDirection = 0f;
+    
+    private float jumpVelocity = 30f;
+    private bool playerJumpQued = false;
+    private float jumpDownTime = 0f;
+    private float jumpDownTimeMax = 0.5f;
 
-    // Start is called before the first frame update
     private void Start()
     {
     }
 
-    // Update is called once per frame
     private void Update()
     {
 
-        PlayerMove();
-        PlayerJump();
-       
+        moveDirection = PlayerMoveDirectionNormalized();
+        playerJumpQued = PlayerJumpQued();
+
+        if (jumpDownTime < jumpDownTimeMax) {
+            jumpDownTime += PlayerJumpStrength();
+        } 
+        
     }
 
-    public void PlayerMove() {
+    private void FixedUpdate() {
+        PlayerMove();
+        PlayerJump();
+    }
+
+    private float PlayerMoveDirectionNormalized() {
         Vector2 inputVector = new Vector2(0, 0);
 
         if (Input.GetKey(KeyCode.D)) {
             inputVector.x += 1;
         }
-
+        
         if (Input.GetKey(KeyCode.A)) {
             inputVector.x -= 1;
         }
 
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, 0f);
-        transform.position += moveDir * Time.deltaTime * moveSpeed;
+        return inputVector.x;
     }
 
-    public void PlayerJump() {
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
-            float jumpVelocity = 30f;
-            playerBody.velocity = Vector2.up * jumpVelocity;
+    private void PlayerMove() {
+        playerBody.linearVelocity = new Vector2(moveDirection * moveSpeed, playerBody.linearVelocityY);
+    }
+
+    private void PlayerJump() {
+        if (playerJumpQued) {
+
+            playerBody.linearVelocity = new Vector2(playerBody.linearVelocityX, jumpVelocity * (jumpDownTime * 2));
+
+            playerJumpQued = false;
+            jumpDownTime = 0f;
+        }
+    }
+
+    private bool PlayerJumpQued() {
+        if (Input.GetKeyUp(KeyCode.Space) && IsGrounded()) {
+            return true;
+        } else if(playerJumpQued) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }   
+
+    private float PlayerJumpStrength() {
+        if (Input.GetKey(KeyCode.Space) && IsGrounded()) {
+            return Time.deltaTime;
+        }
+        else {
+            return 0f;
         }
     }
 
     private bool IsGrounded()
     {
-        float extraHeight = 0.01f;
-        RaycastHit2D raycastHit = Physics2D.CapsuleCast(capsuleCollider2d.bounds.center, capsuleCollider2d.bounds.size, capsuleCollider2d.direction, 0f, Vector2.down, extraHeight, platformLayerMask);
+        float extraHeight = 0.05f;
+        RaycastHit2D raycastHit = Physics2D.CapsuleCast(capsuleCollider2d.bounds.center, 
+            capsuleCollider2d.bounds.size, 
+            capsuleCollider2d.direction, 0f, Vector2.down, 
+            extraHeight, platformLayerMask);
+
         return raycastHit.collider != null;
     }
         
